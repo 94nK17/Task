764b36786c6553ea32324f18ccabcd3863845091
94nK17 <ankitpanwar32@gmail.com>
2020-02-25 22:47:08 +0530

    added  post build script and static files route

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const path = require('path')
 
 const app = express()
 
@@ -20,6 +20,16 @@ app.use('/tasks', require('./routes/TaskRoutes'))
 
 //authRoute
 app.use('/auth', require('./routes/AuthRoutes'))
+
+//serve static assests in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 const Port = process.env.PORT || 5000
